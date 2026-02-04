bcea484bbcca863f249eadf7b777724548450c06
Ronald Sandi <A01794620@tec.mx>
2026-02-04 14:13:30 -0600

    Changes to fix reports of pylint [5]

---
+++ 1_Compute_Statistics/compute_statistics.py
@@ -0,0 +1,280 @@
+"""
+Program #1. Compute Statistics.
+"""
+
+import sys
+import math
+import time
+import errno
+
+
+# Class: Pruebas de Software y Aseguramiento de la Calidad (TC4017)
+# Programming Individual Exercise: 4.2.1 - Error Analysis using Pylint – PEP 8
+# Professor: PhD Gerardo Padilla Zárate
+# Tutor/Evaluator Professor: PhD Daniel Flores Araiza
+# Student / Investigator: Ronald Sandí Quesada – (A01794620)
+# I Trimester – 2026
+# Date: 01/02/2026
+
+
+#Requirements:
+# Req 1. The program shall be invoked from a command line.
+#        The program shall receive a file as parameter.
+#        The file will contain a list of items (presumable numbers).
+# Req 2. The program shall compute all descriptive statistics from a file containing numbers.
+#        The results shall be print on a screen and on a file named StatisticsResults.txt.
+#        All computation MUST be calculated using the basic algorithms, not functions or libraries.
+#        The descriptive statistics are mean, median, mode, standard deviation, and variance.
+# Req 3. The program shall include the mechanism to handle invalid data in the file.
+#        Errors should be displayed in the console and the execution must continue.
+# Req 4. The name of the program shall be compute_statistics.py
+# Req 5. The minimum format to invoke the program shall be as follows:
+#        python compute_statistics.py fileWithData.txt
+# Req 6. The program shall manage files having from hundreds of items to thousands of items.
+# Req 7. The program should include at the end of the execution the time elapsed
+#        for the execution and calculus of the data. This number shall be included in the
+#        results file and on the screen.
+# Req 8. Be compliant with PEP8.
+
+
+NA = "n/a"
+ZERO_WHEN_NULL = 0.0
+RESOURCE_PATH = "P1\\"
+
+def standard_deviation(number_list_):
+    """
+    Calculates the Standard Deviation (SD) of a list of numbers.
+
+    Args:
+        number_list_ (float): The source list of numbers to be used as
+        the SD raw material.
+
+    Returns:
+        float: SD calculated.
+    """
+    variance_ = variance(number_list_)
+    standard_deviation_ = variance_ ** 0.5
+    return standard_deviation_
+
+
+def variance(number_list_):
+    """
+    Calculates the variance of a list of numbers.
+
+    Args:
+        number_list_ (float[]): The source list of numbers to be used as
+        the calculation raw material.
+
+    Returns:
+        float: calculated variance.
+    """
+    mean_ = mean(number_list_)
+    variance_sum = 0.0
+
+    for number in number_list_:
+        if not math.isnan(number):
+            variance_item = (number - mean_) ** 2
+            variance_sum += variance_item
+
+    variance_ = variance_sum / (len(number_list_) -1)
+    return variance_
+
+def mode(number_list_):
+    """
+    Calculates the Mode of a list of numbers.
+
+    Args:
+        number_list_ (float[]): The source list of numbers to be used as
+        the calculation raw material.
+
+    Returns:
+        string: mode calculated.
+    """
+    mode_counter = {}
+
+    for number in number_list_:
+        if not math.isnan(number):
+            if number in mode_counter:
+                mode_counter[number] += 1
+            else:
+                mode_counter[number] = 1
+
+    mode_counter_sorted = dict(sorted(mode_counter.items(), key=lambda item: item[1], reverse=True))
+    mode_, mode_hits = (next(iter(mode_counter_sorted.items())))
+
+    if mode_hits >=2:
+        mode_ = str(mode_)
+    else:
+        mode_ = NA
+
+    return mode_
+
+
+def median(number_list_):
+    """
+    Calculates the Median of a list of numbers.
+
+    Args:
+        number_list_ (float[]): The source list of numbers to be used as
+        the calculation raw material.
+
+    Returns:
+        float: Median calculated.
+    """
+    median_ = 0.0
+    position_to_take = 0
+
+    median_list = [] #number_list_[:]
+
+    for number in number_list_:
+        if not math.isnan(number):
+            median_list.append(number)
+        else:
+            print("Horseman...")
+
+    median_list.sort()
+
+    length = len(median_list)
+
+    if length % 2 == 0:
+        # Even
+        position_to_take = int(length / 2) - 1
+        median_ = (median_list[position_to_take] + median_list[position_to_take + 1]) / 2
+    else:
+        # Odd
+        position_to_take = int((length + 1) / 2) - 1
+        median_ = median_list[position_to_take]
+
+    return median_
+
+
+def mean(number_list_):
+    """
+    Calculates the Mean of a list of numbers.
+
+    Args:
+        number_list_ (float[]): The source list of numbers to be used as
+        the calculation raw material.
+
+    Returns:
+        float: Mean calculated.
+    """
+    sum_ = 0.0
+    mean_= 0.0
+
+    for number in number_list_:
+        if not math.isnan(number):
+            sum_ = sum_ + number
+
+    mean_ = sum_ / len(number_list_)
+    return mean_
+
+
+def file_lines_to_float(file_lines_, include_nil_counts=True):
+    """
+    Convert a String List to Float Point List.
+    The non-numeric values are not converted.
+    The flag included_nil_counts define either or not to include the NA item in the list.
+
+    Args:
+        file_lines_ (Sting[]): The source list of lines in the file as String List.
+        include_nil_counts (Boolean): Flag that defines either to include the NA item in the list.
+
+    Returns:
+        float (Float[]): Numbers converted as a List.
+    """
+    number_list_ = []
+    for number_in_line in file_lines_:
+        try:
+            number_from_string = float(number_in_line)
+            number_list_.append(number_from_string)
+        except ValueError as e:
+            print(f"Error: Unable to convert '{number_in_line}' to a float. Details: {e}")
+
+            if include_nil_counts:
+                number_list_.append(float('nan'))
+
+    return number_list_
+
+
+def read_from_file(file_path_):
+    """
+    Read the context of a plain file.
+
+    Args:
+        file_path_ (strig): File path to be read.
+
+    Returns:
+        string[]: The content of the file in lines separation.
+    """
+    source_file_lines = []
+
+    try:
+        with open(file_path_, 'r', encoding="utf-8") as file:
+            for line in file:
+                source_file_lines.append(line.strip())
+
+        return source_file_lines
+    except EnvironmentError as err:
+        if err.errno == errno.ENOENT:
+            print(
+                f"File not found Exception when trying to load the file '{file_path_}'"
+                ".\nDouble Check the file path and try again.")
+        else:
+            print(
+                f"There is a system problem to read the file '{file_path_}'."
+                "\nDouble Check the file path and try again.")
+        return source_file_lines
+
+
+def print_results(number_list_):
+    """
+    Print the computations results.
+
+    Args:
+        number_list_ (float[]): The source list of numbers to be used as
+        the print raw material.
+
+    Returns:
+        void: System print by console.
+    """
+    length = len(number_list_)
+    head_ = "=-" * 20
+    results_to_print = f"{head_}\n" \
+                       f"Count: {length}\n" \
+                       f"Mean: {mean(number_list_)}\n" \
+                       f"Median: {median(number_list_)}\n" \
+                       f"Mode: {mode(number_list_)}\n" \
+                       f"Standard Deviation: {standard_deviation(number_list_)}\n" \
+                       f"Variance: {variance(number_list_)}\n" \
+                       f"{head_}\n"
+
+    print(results_to_print)
+
+def print_help():
+    head_ = "=-=" * 30
+    results_to_print = f"{head_}\n" \
+                       f"This program requires one parameter indicating the file to be processed.\n" \
+                       f"Additional parameters will be ignored.\n" \
+                       f"Invocation example:\n\tpython compute_statistics.py fileWithData.txt\n" \
+                       f"{head_}\n"
+
+    print(results_to_print)
+
+
+# Main Execution Point
+if __name__ == '__main__':
+    start_time = time.perf_counter()
+
+    if len(sys.argv) > 1:
+        file_to_proces = sys.argv[1]
+        file_lines = read_from_file(f"{RESOURCE_PATH}{file_to_proces}")
+        number_list = file_lines_to_float(file_lines, True)
+        if len(number_list)>=1:
+            print_results(number_list)
+    else:
+        print_help()
+
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time} seconds")
